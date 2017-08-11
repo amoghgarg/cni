beca01912a43ae7c74adb6fa3e52bba2b8ff6ad1
amogh <amoghgarg1990@gmail.com>
2017-08-11 15:51:48 +0900

    gitignore, minor script changes

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,9 +4,14 @@ contid=$(docker run -d --net=none busybox:latest /bin/sleep 10000000)
 pid=$(docker inspect -f '{{ .State.Pid }}' $contid)
 netnspath=/proc/$pid/ns/net
 
-echo docker container: $contid
-echo pid: $pid
-echo netnspath: $netnspath
+echo "Container created with id: $(echo $contid | cut -c1-10)"
+echo ""
 
 echo '{"cniVersion": "0.2.0","name": "mynet","type": "bridge","bridge": "cni0","isGateway": true,"ipMasq": true,"ipam":{"type": "host-local","subnet": "172.18.0.0/16","routes": [{ "dst": "0.0.0.0/0" }]}}' \
   | sudo CNI_COMMAND=ADD CNI_NETNS=$netnspath CNI_CONTAINERID=$contid ./bin/bridge
+
+
+echo "-----------------------------------------------------------------"
+echo "You can try pinging the container on the address mentioned above."
+echo "You can stop the container with this command:"
+echo "    sudo scripts/stop.sh $(echo $contid | cut -c1-10)"
